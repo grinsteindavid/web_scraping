54a37fcd5aca598e1f3a957c1480830267df0a29
david <davidmirandagrinstein@gmail.com>
2020-12-16 14:20:53 -0300

    some code refactor

--- main.js
+++ main.js
@@ -1,41 +1,18 @@
 const puppeteer = require('puppeteer');
-// const puppeteer = require('puppeteer-extra');
-// const userAgent = require('user-agents');
-// const randomUseragent = require('random-useragent');
-// const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-// puppeteer.use(StealthPlugin());
 
 const DNI = process.argv[2];
 
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-
-    //Randomize User agent or Set a valid one
-    // const userAgent = randomUseragent.getRandom();
-    // const UA = userAgent || USER_AGENT;
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     try {
-        //Randomize viewport size
-        // await page.setViewport({
-        //     width: 600,
-        //     height: 400,
-        //     deviceScaleFactor: 1,
-        //     hasTouch: false,
-        //     isLandscape: false,
-        //     isMobile: false,
-        // });
-
-        // await page.setUserAgent(UA);
-        // await page.setJavaScriptEnabled(true);
-        // page.setDefaultNavigationTimeout();
         await page.goto('http://sodoc.embaven.cl/consultaprorroga');
 
         await page.waitForTimeout(3000);
 
         await page.waitForSelector('input[name=dni]');
-        // await page.$eval('input[name=dni]', el => el.value = '123');
         await page.type('input[name=dni]', DNI);
         const dniInput = await page.$('input[name=dni]');
         const dniInputvalue = await page.evaluate(element => element.value, dniInput);
@@ -45,18 +22,6 @@ const DNI = process.argv[2];
         console.log('waiting for .#consultaProrroga');
         await page.waitForSelector('#consultaProrroga');
         await page.click('#consultaProrroga');
-
-
-        // console.log('waiting for response /existe');
-        // await page.waitForResponse(response => {
-        //     response.url() === 'http://sodoc.embaven.cl/consultaprorroga/existe' && response.status() === 200;
-        // });
-
-        // await page.waitForSelector('#swal2-content');
-        // const swal2Content = await page.$('#swal2-content');
-        // const swal2TextContent = await page.evaluate(element => element.textContent, swal2Content);
-
-        // console.log({ swal2TextContent });
 
         console.log('waiting for .adultos.cursor');
 
@@ -82,7 +47,8 @@ const DNI = process.argv[2];
         const swal2ContentConfirmation = await page.$('#swal2-content');
         const swal2TextContentConfirmation = await page.evaluate(element => element.textContent, swal2ContentConfirmation);
 
-        console.log({ swal2TextContentConfirmation });
+        const isConfirmed = swal2TextContentConfirmation !== 'Su prórroga aún no esta disponible para ser retirada en ésta misión diplomática. Consulte la próxima semana';
+        console.log({ swal2TextContentConfirmation, isConfirmed });
 
     } catch (error) {
         console.error(error);
